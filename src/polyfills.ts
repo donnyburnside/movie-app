0110bad219a4ebbf49ab5b45618db3660789efa8
Donny Burnside <donnyburnside1@gmail.com>
2018-04-16 01:03:48 +0100

    Added browsers polyfills and Angular dependencies

--- src/polyfills.ts
+++ src/polyfills.ts
@@ -1,1 +1,50 @@
+/**
+ * This file includes polyfills needed by Angular and is loaded before the app.
+ *
+ * This file is divided into 2 sections:
+ *    Browser polyfills. These are applied before loading ZoneJS and are sorted
+ *    by browsers.
+ *    Application imports. Files imported after ZoneJS that should be loaded
+ *    before your main file.
+ *
+ * The current setup is for so-called "evergreen" browsers; the last versions of
+ * browsers that automatically update themselves. This includes Safari >= 10,
+ * Chrome >= 55 (including Opera), Edge >= 13 on the desktop, and iOS 10 and
+ * Chrome on mobile.
+ * Learn more in https://angular.io/docs/ts/latest/guide/browser-support.html
+ */
+
+/**
+ * Browser Polyfills
+ * IE9, IE10 and IE11 requires all of the following polyfills.
+ */
+import 'core-js/es6/symbol';
+import 'core-js/es6/object';
+import 'core-js/es6/function';
+import 'core-js/es6/parse-int';
+import 'core-js/es6/parse-float';
+import 'core-js/es6/number';
+import 'core-js/es6/math';
+import 'core-js/es6/string';
+import 'core-js/es6/date';
+import 'core-js/es6/array';
+import 'core-js/es6/regexp';
+import 'core-js/es6/map';
+import 'core-js/es6/weak-map';
+import 'core-js/es6/set';
+
+
+/**
+ * Evergreen Polyfills
+ * Evergreen browsers require these.
+ */
+import 'core-js/es6/reflect';
+import 'core-js/es7/reflect';
+import 'core-js/es7/array';
+
+
+/**
+ * Angular Polyfills
+ * Functionality required by Angular itself.
+ */
 import 'zone.js/dist/zone';
